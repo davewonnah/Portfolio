49bff4257ef6fa3a8d13c76149ebdbb5a62ec5af
Dave Wonnah <wonnahdave@gmail.com>
2023-05-23 10:19:09 +0000

    working on menu functionality

--- apps.js
+++ apps.js
@@ -1,1 +1,7 @@
-aaka;
+const menu_container = document.querySelector(".menu");
+const menu_img = document.querySelector(".menu-line");
+const dropDown = document.querySelector(".dropDown-menu");
+
+menu_container.onclick = function () {
+    dropDown.classList.toggle('open')
+}
